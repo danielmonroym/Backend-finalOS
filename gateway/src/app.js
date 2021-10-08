18a7785279b863318112affc6e1c672ff71bf8e0
ViDaMora <morajaramillov@gmail.com>
2021-10-08 17:17:55 -0500

    Union de todos los back

--- gateway/src/app.js
+++ gateway/src/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 const usersRouter = require('./users/users.routes');
-<<<<<<< HEAD
-const inventaryRouter = require('./inventary/inventary.routes');
-=======
 const shipingRouter = require('./shiping/shiping.routes')
->>>>>>> ac203944f444524b67f10db325f09d25bab26d5b
+const inventaryRouter = require('./inventary/inventary.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -17,12 +14,7 @@ app.get('/', (req, res) => {
   });
 
 
-<<<<<<< HEAD
   app.use('/api/users', usersRouter);
-  app.use('/api/inventory', inventoryRouter);
-  module.exports = app;
-=======
-app.use('/api/users', usersRouter);
-app.use('/api/shiping', shipingRouter);
+  app.use('/api/inventory', inventaryRouter);
+  app.use('/api/shiping', shipingRouter);
 module.exports = app;
->>>>>>> ac203944f444524b67f10db325f09d25bab26d5b
